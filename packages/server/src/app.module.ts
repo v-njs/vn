7ba5cb19888d2f0ecbe1119c2beb46af5c360474
mmmmr <ob12564@163.com>
2024-03-17 23:05:43 +0800

    feat: user && role && permission

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
 
 @Module({
   imports: [
@@ -18,12 +22,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       logging: true,
       // 数据库连接池中连接的最大数量
       poolSize: 10,
-      entities: [],
+      entities: [User, Role, Permission],
       connectorPackage: 'mysql2',
       extra: {
         authPlugins: 'sha256_password',
       },
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ packages/server/src/user/user.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get('init')
+  async initData() {
+    await this.userService.initData();
+    return '初始化成功';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+
+  @Post('login')
+  async login(@Body() loginUser: UserLoginDto) {
+    const user = await this.userService.login(loginUser);
+    console.log(user);
+    return 'success';
+  }
+}
